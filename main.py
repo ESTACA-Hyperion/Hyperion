38088839e76e69a09aba659faae72af1f41a25f1
A-FitzGerald <avon.fitzy@gmail.com>
2019-11-07 17:30:35 +0100

    Started working on the vector impl

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-# Testing
+import numpy as np
+from xmath.vector import Vector
+
 print("Hello World!")
+
+accel = [
+    0,
+    1,
+    1,
+]
+accel2 = [
+    1,
+    1,
+    1,
+]
+
+print(accel + accel2)
---
+++ xmath/vector.py
@@ -0,0 +1,12 @@
+import math
+
+
+class Vector:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
